51484042a20fcb48150e5980bd049a644c437560
Cristian Barrero <barrerocristian609@gmail.com>
2024-11-09 22:07:38 -0500

    Guia 6

---
+++ infija&posfija/src/back/Nodo.java
@@ -0,0 +1,12 @@
+
+package back;
+
+public class Nodo {
+    public char dato;
+    public Nodo siguiente;
+    
+    public Nodo(char dato){
+        this.dato = dato;
+        this.siguiente =null;
+    }
+}
